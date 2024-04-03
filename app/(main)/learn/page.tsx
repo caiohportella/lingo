f76026c8a25b9ee6cbfa1585bc632bcc435a63a2
Caio Portella <caiohportella@gmail.com>
2024-04-03 17:21:54 -0300

    fixed lesson progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,11 +2,18 @@ import FeedWrapper from "@/components/feed-wrapper";
 import StickyWrapper from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import UserProgress from "@/components/user-progress";
-import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress, getUserSubscription } from "@/db/queries";
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+  getUserSubscription,
+} from "@/db/queries";
 import { redirect } from "next/navigation";
 import Unit from "./unit";
 import { Promo } from "@/components/promo";
 import { Quests } from "@/components/quests";
+import { lessons, units as unitsSchema } from "@/db/schema";
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
@@ -15,7 +22,13 @@ const LearnPage = async () => {
   const lessonPercentageData = getLessonPercentage();
   const userSubscriptionData = getUserSubscription();
 
-  const [userProgress, units, courseProgress, lessonPercentage, userSubscription] = await Promise.all([
+  const [
+    userProgress,
+    units,
+    courseProgress,
+    lessonPercentage,
+    userSubscription,
+  ] = await Promise.all([
     userProgressData,
     unitsData,
     courseProgressData,
@@ -29,7 +42,7 @@ const LearnPage = async () => {
     redirect("/courses");
   }
 
-  if(!courseProgress) redirect("/courses");
+  if (!courseProgress) redirect("/courses");
 
   return (
     <div className="flex gap-[48px] px-6">
@@ -43,7 +56,13 @@ const LearnPage = async () => {
               description={unit.description}
               title={unit.title}
               lessons={unit.lessons}
-              activeLesson={courseProgress.activeLesson}
+              activeLesson={
+                courseProgress.activeLesson as
+                  | (typeof lessons.$inferSelect & {
+                      unit: typeof unitsSchema.$inferSelect;
+                    })
+                  | undefined
+              }
               activeLessonPercentage={lessonPercentage}
             />
           </div>
--- scripts/prod.ts
+++ scripts/prod.ts
@@ -10,7 +10,7 @@ const db = drizzle(sql, { schema });
 
 const main = async () => {
   try {
-    console.log("Seeding database");
+    console.log("Seeding database...");
 
     // Delete all existing data
     await Promise.all([
@@ -325,7 +325,7 @@ const main = async () => {
         }
       }
     }
-    console.log("Database seeded successfully");
+    console.log("Database seeded successfully!");
   } catch (error) {
     console.error(error);
     throw new Error("Failed to seed database");
